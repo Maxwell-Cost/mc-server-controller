50e185c5d112ceb3fbcd204af9c1c3cb6cdde4da
Maxwell-cost <maxwellacostantino@gmail.com>
2026-06-23 11:44:31 -0400

    Reworks config file again to seperate live config from example

--- config.example.py
+++ config.example.py
@@ -1,9 +1,11 @@
 # Configuration file for Minecraft server controller
 
+# Paste this entire file into config.py and edit the values below to match your AMP instance and Discord webhook.
+
 DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/00000/abcde1234"
 INSTANCE_NAME = "friendly_name_of_your_instance"
 
-AMP_URL = "http://192.168.1.5:8080"
+AMP_URL = "https://your_amp_url.com"
 AMP_USER = "your_amp_username"
 AMP_PASS = "your_amp_password"
 
